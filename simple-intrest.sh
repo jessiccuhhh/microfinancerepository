c0494ff54e573d0c1f239117f7137f2cc25f7801
jessiccuhhh <jessiccuhhh@icloud.com>
2025-06-29 21:45:17 -0400

    Update simple-intrest.sh
    Added Code

--- simple-intrest.sh
+++ simple-intrest.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
+# This script calculates simple intrest given principal,
+#annual rate of intrest and time period in years
 
+# Do not use this in producation. Sample purpose only
+
+# Author: Upkar Lidder (IBM)
+# Additional Authors:
+# jessiccuhhh
+
+# Input:
+# p, principal amount
+# t, time period in years
+# r, annual rate of intrest
+
+# Output:
+# simple intrest = p*t*r
+
+echo "Enter the principal"
+read p
+echo "Enter rate of intrest per year"
+read r
+echo "Enter time period in years"
+read t
+
+s= expr $p \* $t \* $r / 100
+echo "The simple intrest is: "
+echo $s
